0b80da26a75b912f2eebf8979cf14fd3140751da
chiucpw <chiujack5354@gmail.com>
2022-12-29 22:38:48 +0800

    final project

---
+++ final project_1/final project_1/final project_1.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE* fptr;
+	char ch;
+
+	fptr = fopen("article.txt", "r");
+	if (fptr != NULL)
+	{
+		while ((ch = getc(fptr)) != EOF)
+		{
+			printf("%c", ch);
+		}
+		fclose(fptr);
+	}
+}
