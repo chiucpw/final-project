ca20c133e7f3bfe885698d59cf197bc346ffabd5
Leeshinhung <Howardlee123999@gmail.com>
2023-01-02 23:06:03 +0800

    final project

---
+++ final project_1/final project_1/final project_1.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define LENGTH 1000
+
+int main()
+{
+	int count = 1, wpm, n, c = 0, r;
+	FILE* fptr;
+	char ch, name[30], nu;
+	char article[LENGTH];
+
+	srand(time(NULL));
+	r = rand() % 10 + 1;
+	sprintf(name, "%d.txt", r);
+	fptr = fopen(name, "r");
+	if (fptr != NULL)
+	{
+		while (fgets(article, LENGTH, fptr) != NULL)
+		{
+			printf("%s", article);
+
+		}
+		fclose(fptr);
+	}
+}
