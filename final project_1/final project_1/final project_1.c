7298e832c519f4b36ed3a9f16f7268e4d5017f29
chiucpw <chiujack5354@gmail.com>
2023-01-03 23:40:06 +0800

    final project

--- final project_1/final project_1/final project_1.c
+++ final project_1/final project_1/final project_1.c
@@ -1,17 +1,26 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
+#define LENGTH 1000
 
 int main()
 {
+	int count = 1, wpm, n, c = 0, r;
 	FILE* fptr;
-	char ch;
+	char ch, name[30], nu;
+	char article[LENGTH];
 
-	fptr = fopen("article.txt", "r");
+	srand(time(NULL));
+	r = rand() % 10 + 1;
+
+	sprintf(name, "%d.txt", r);
+	fptr = fopen(name, "r");
 	if (fptr != NULL)
 	{
-		while ((ch = getc(fptr)) != EOF)
+		while (fgets(article, LENGTH, fptr) != NULL)
 		{
-			printf("%c", ch);
+			printf("%s", article);
+
 		}
 		fclose(fptr);
 	}
